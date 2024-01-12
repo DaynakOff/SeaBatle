f59b47db4db465c8690e5086e11f5f6f0b3e26dc
DaynakOff <aleksandr.daynakov@mail.ru>
2024-01-12 20:07:05 +0700

    working version

--- main.py
+++ main.py
@@ -37,14 +37,18 @@ class Ship:
 
 class Board:
 	def __init__(self):
-		self.board = [['0'] * 6 for _ in range(6)]
+		self.board = [['0' for _ in range(6)] for _ in range(6)]
 		self.ships = []
 		self.hid = False
 		self.live_ships = 0
+		self.contoured = [[False for _ in range(6)] for _ in range(6)]
+		self.missed = [[False for _ in range(6)] for _ in range(6)]
+		self.destroyed = [[False for _ in range(6)] for _ in range(6)]
+
 
 	def add_ship(self, ship):
 		for dot in ship.dots():
-			if self.out(dot) or self.board[dot.x][dot.y] == '■':
+			if self.out(dot) or self.board[dot.x][dot.y] == '■' or self.board[dot.x][dot.y] == '.':
 				raise BoardOutException('Невозможно установить корабль')
 		self.ships.append(ship)
 		for dot in ship.dots():
@@ -60,22 +64,26 @@ class Board:
 		for row in range(6):
 			print(f'{row+1}|', end='')
 			for col in range(6):
-				if self.hid and self.board[row][col] == '■':
+				if self.hid and self.board[row][col] == '■' and not any(
+						[s.health_point for s in self.ships if Dot(row, col) in s.dots()]):
+					print('Т|', end='')
+				elif self.hid and not self.destroyed[row][col] and (
+						self.board[row][col] == '■' or (self.contoured[row][col] and not self.missed[row][col])):
 					print('0|', end='')
 				else:
 					print(f'{self.board[row][col]}|', end='')
 			print()
 
-	def contour(self, ship):
-		if not self.hid:
-			for dot in ship.dots():
-				for j in range(dot.y-1, dot.y + 2):
-					for i in range(dot.x-1, dot.x + 2):
-						if not self.out(Dot(j, i)):
-							if self.board[i][j] == '0':
-								self.board[i][j] = '.'
-		else:
-			return
+	def contour(self, ship, destroy=False):
+		for dot in ship.dots():
+			for j in range(dot.y-1, dot.y + 2):
+				for i in range(dot.x-1, dot.x + 2):
+					if not self.out(Dot(i, j)):
+						if self.board[i][j] == '0':
+							self.board[i][j] = '.' if not destroy else 'Т'
+							self.contoured[i][j] = True
+							if destroy:
+								self.destroyed[i][j] = True
 
 	def out(self, dot):
 		return dot.x < 0 or dot.x >= 6 or dot.y < 0 or dot.y >= 6
@@ -83,7 +91,7 @@ class Board:
 	def shot(self,dot):
 		if self.out(dot):
 			raise BoardOutException('Не стреляй мимо поля')
-		if self.board[dot.x][dot.y] == 'X' or self.board[dot.x][dot.y] == 'T':
+		if self.board[dot.x][dot.y] == 'X' or self.board[dot.x][dot.y] == 'Т':
 			raise BoardOutException('Сюда уже стреляли')
 
 		for ship in self.ships:
@@ -92,13 +100,16 @@ class Board:
 				ship.health_point -= 1
 				if ship.health_point == 0:
 					self.live_ships -= 1
-					self.contour(ship)
+					self.contour(ship, True)
+					for d in ship.dots():
+						self.destroyed[d.x][d.y] = True
 					print('Корабль уничтожен')
 					return True
 				else:
 					print('Попал, стреляй еще')
 					return True
-		self.board[dot.x][dot.y] = 'T'
+		self.board[dot.x][dot.y] = 'Т'
+		self.missed[dot.x][dot.y] = True
 		print('Промахнулся...')
 		return False
 
@@ -111,7 +122,7 @@ class Player:
 	def ask(self):
 		pass
 
-	def move(self):
+	def move(self, enemy_board):
 		while True:
 			try:
 				dot = self.ask()
@@ -126,7 +137,7 @@ class Player:
 
 class AI(Player):
 	def ask(self):
-		dot = (random.randint(0, 5),random.randint(0, 5))
+		dot = Dot(random.randint(0, 5),random.randint(0, 5))
 		return dot
 
 
@@ -134,7 +145,7 @@ class User(Player):
 	def ask(self):
 		x = int(input("Введите номер строки:"))
 		y = int(input("Введите номер столбца:"))
-		return Dot(x, y)
+		return Dot(x-1, y-1)
 
 
 class Game:
@@ -143,8 +154,12 @@ class Game:
 		self.user_board = Board()
 		self.player_ai = AI()
 		self.ai_board = Board()
-		self.random_board(self.player_user.enemy_board)
-		self.random_board(self.player_ai.enemy_board)
+		self.player_ai.own_board.hid = True
+		self.random_board(self.player_user.own_board)
+		self.random_board(self.player_ai.own_board)
+		self.player_user.enemy_board = self.player_ai.own_board
+		self.player_ai.enemy_board = self.player_user.own_board
+
 
 	def random_board(self, board):
 		self.ships = [3, 2, 2, 1, 1, 1]
@@ -165,7 +180,55 @@ class Game:
 
 
 	def greet(self):
-		
+		print("Привет!!! Добро пожаловать в игру : МОРСКОЙ БОЙ!")
+		print("В игре учавствует 6 кораблей \n"
+		      "1 - трехпалубный \n"
+		      "2 - двухпалубных \n"
+		      "3 - однопалубных")
+		print("Для осуществления выстрела необходимо ввести номер строки от 1 до 6 \n"
+		      "И номер столбца в том же диапазоне.")
+		print("Побеждает игрок первым уничтоживший все вражеские корабли!\n"
+		      "Удачи в бою АДМИРАЛ!!!")
+
+
+	def loop(self):
+		while True:
+			print('Ваша доска:')
+			self.player_user.own_board.display()
+			print('Доска противника:')
+			self.player_user.enemy_board.display()
+			while self.player_user.move(self.player_ai.own_board):
+				print('Ваша доска:')
+				self.player_user.own_board.display()
+				print('Доска противника:')
+				self.player_user.enemy_board.display()
+			if self.player_ai.own_board.live_ships == 0:
+				print("Пользователь выиграл!")
+				break
+			print('Доска ИИ:')
+			self.player_ai.own_board.display()
+			print('Ваша доска:')
+			self.player_user.own_board.display()
+			while self.player_ai.move(self.player_user.own_board):
+				print('Доска ИИ:')
+				self.player_ai.own_board.display()
+				print('Ваша доска:')
+				self.player_user.own_board.display()
+			if self.player_user.own_board.live_ships == 0:
+				print("ИИ выиграл!")
+				break
+
+	def start(self):
+		self.greet()
+		self.loop()
+
+
+s1 = Game()
+s1.start()
+
+
+
+
 
 
 
